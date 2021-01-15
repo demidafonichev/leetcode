1048eee64579dc9d56a308da12b28111fc4b598d
Demid Afonichev <demidafonichev@gmail.com>
2021-01-15 13:39:45 +0300

    complete 2021_January/week1

--- challenges/2021_January/week1/2_find_a_corresponding_node/main.cpp
+++ challenges/2021_January/week1/2_find_a_corresponding_node/main.cpp
@@ -1,21 +1,30 @@
-#include <iostream>
-
+/**
+ * Definition for a binary tree node.
+ */
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x) : val(x), left(), right() {}
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
+    TreeNode* ans;
+    TreeNode* target;
 public:
+    void inorder(TreeNode* o, TreeNode* c) {
+        if (o != NULL) {
+            inorder(o->left, c->left);
+            if (o == this->target) {
+                ans = c;
+            }
+            inorder(o->right, c->right);
+        }
+    }
+    
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        return nullptr;
+        this->target = target;
+        inorder(original, cloned);
+        return this->ans;
     }
 };
-
-int main() {
-    std::cout << ("hello");
-
-    return 0;
-}
